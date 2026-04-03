536862ddd68a28dee146272efb0932c6fe7bbc55
krishna-mw-dev <krishna.mw.dev@gmail.com>
2026-04-03 05:41:35 +0000

    variables learned

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 12345
+let accountlEmail = "krishna@gmail.com"
+var accountPassword = "12345"
+accountCity = "meerut"
+let accountState;
+let name1;
+// accountId = 2 
+console.log(accountId)
+
+accountEmail = "abab@gmail.com"
+accountPassword = "2222"
+accountCity = "jaipur"
+
+console.table( [accountId , accountEmail , accountPassword , accountCity , accountState , name1] )
